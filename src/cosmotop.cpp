2bc14bbe36661b783d9d679fab29ac731661d1c8
Brett Jia <dev.bjia56@gmail.com>
2025-01-19 00:01:14 -0500

    prepare v0.3.0 release

--- src/cosmotop.cpp
+++ src/cosmotop.cpp
@@ -78,7 +78,7 @@ namespace Global {
 		{"#062c43", "██████╗ ██████║ ██████║ ██║     ██║ ██████║   ██║   ██████║ ██║"},
 		{"#000000", "╚═════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝ ╚═════╝   ╚═╝   ╚═════╝ ╚═╝"},
 	};
-	const string Version = "0.2.0";
+	const string Version = "0.3.0";
 
 	int coreCount;
 	string overlay;
