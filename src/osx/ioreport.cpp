6a9cb6daee6e9d1467dba0e465273e55a371ba4d
Brett Jia <dev.bjia56@gmail.com>
2025-01-31 11:10:31 -0500

    initial scaffolding for ioreport subscriptions

---
+++ src/osx/ioreport.cpp
@@ -0,0 +1,15 @@
+
+
+#include "ioreport.hpp"
+
+namespace Cpu {
+	IOReportSubscription::IOReportSubscription() {
+	}
+
+	IOReportSubscription::~IOReportSubscription() {
+	}
+
+	long long IOReportSubscription::getANEEnergy() {
+		return 0;
+	}
+}  // namespace Cpu
