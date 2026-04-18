15c580ed0f750cd23e89fb57a141bbd868b5400b
Brett Jia <dev.bjia56@gmail.com>
2026-04-17 21:46:26 -0400

    replace deprecated xterm packages with scoped versions
    Migrate desktop frontend imports and dependencies from xterm/xterm-addon-fit to @xterm/xterm and @xterm/addon-fit while keeping terminal behavior unchanged.

--- desktop/frontend/src/main.js
+++ desktop/frontend/src/main.js
@@ -1,8 +1,8 @@
-import "xterm/css/xterm.css";
+import "@xterm/xterm/css/xterm.css";
 import "./style.css";
 
-import { Terminal } from "xterm";
-import { FitAddon } from "xterm-addon-fit";
+import { Terminal } from "@xterm/xterm";
+import { FitAddon } from "@xterm/addon-fit";
 import {
   IsRunning,
   Resize,
